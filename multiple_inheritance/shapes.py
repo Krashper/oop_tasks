5d7e34fe4e1954603d17596e7b5eb42616aeacc8
Solarev Ruslan <ruslan251207@gmail.com>
2023-03-06 20:26:02 +1000

    added shapes.py classes

--- multiple_inheritance/shapes.py
+++ multiple_inheritance/shapes.py
@@ -2,5 +2,28 @@ from math import pi
 import abc
 
 class Shape(abc.ABC):
+    @abc.abstractclassmethod
     def area():
         raise NotImplementedError('Нельзя вызывать абстрактный метод')
+    @abc.abstractclassmethod
+    def perimeter():
+        raise NotImplementedError('Нельзя вызывать абстрактный метод')
+
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+    def area(self):
+        return self.length * self.width
+    def perimeter(self):
+        return (self.length + self.width) * 2
+
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+    def area(self):
+        return self.radius ** 2 * pi
+    def perimeter(self):
+        return 2 * self.radius * pi
